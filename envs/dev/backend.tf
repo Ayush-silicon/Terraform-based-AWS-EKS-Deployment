a61ade592ebcfca2c3ba54310a5eb8dbec05804d
Ayush Singh <154998285+Ayush-silicon@users.noreply.github.com>
2025-08-01 00:18:27 +0530

    Delete envs/dev directory

--- envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-tf-state-bucket"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-state-lock"
-  }
-}
